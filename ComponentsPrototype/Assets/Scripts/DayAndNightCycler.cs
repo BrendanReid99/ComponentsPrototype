8b9314a48bcbc305b948fb7f55d3c002399de754
Brendan Reid <36493852+BrendanReid99@users.noreply.github.com>
2023-03-17 13:15:28 +0000

    added time of day UI

--- ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
+++ ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using System.Diagnostics.Tracing;
 
 public class DayAndNightCycler : MonoBehaviour
 {
@@ -10,6 +13,11 @@ public class DayAndNightCycler : MonoBehaviour
     private float rotationAngleStep;
     private Vector3 rotationAxis;
 
+    public TextMeshProUGUI timeOfDay;
+    public float currentTime;
+    int counter;
+
+
     private void Awake()
     {
         //apply initial rotation on the stars
@@ -18,12 +26,29 @@ public class DayAndNightCycler : MonoBehaviour
         //compute relevant calculation parameters
         starsRefreshRate = 0.1f;
         rotationAxis = starsTransform.right;
-        rotationAngleStep = 360.0f * starsRefreshRate / gameParameters.lengthOfDayInSeconds; 
+        rotationAngleStep = 360.0f * starsRefreshRate / gameParameters.lengthOfDayInSeconds;
     }
 
-    private void Start()
+    void Start()
     {
+        //set current time in relation to DAY/NIGHT Ratio
+        currentTime = 360;
+
+
         StartCoroutine("UpdateStars");
+    }
+
+    void FixedUpdate() //EVERY 2 FRAMES == 1 MINUTE (GAME TIME)
+    {
+        counter += 1;
+        if (counter % 4 == 0) { //modulo 4 here as length of day is set to 120 (2 minutes), if length of day was set to 60 (1 minute) we would change to modulo 2 (as fixedUpdate is every 2 frames)
+            currentTime += 1;   //update the current time by 1 minute
+        }
+
+        float hours = Mathf.FloorToInt(currentTime / 60);
+        float minutes = Mathf.FloorToInt(currentTime % 60);
+        timeOfDay.text = string.Format("{0:00}:{1:00}", hours,  minutes); //converts text into hours and minutes format
+
     }
 
     private IEnumerator UpdateStars()
