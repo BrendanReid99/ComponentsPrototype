f67cde0cf6049b82eeabeef3cbe623d61c8d5eff
Brendan Reid <36493852+BrendanReid99@users.noreply.github.com>
2023-03-21 18:52:20 +0000

    colliders for fencing added + edited time to be 6am

--- ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
+++ ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
@@ -32,7 +32,7 @@ public class DayAndNightCycler : MonoBehaviour
     void Start()
     {
         //set current time in game (360 minutes = 6am in game time i.e., 60 (00:00am) * 6 (hrs) = 360)
-        currentTime = 1200;
+        currentTime = 360;
 
 
         StartCoroutine("UpdateStars");
