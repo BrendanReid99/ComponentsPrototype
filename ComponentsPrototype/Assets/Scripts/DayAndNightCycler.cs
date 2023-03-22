d71bb7f668e648fc71cf667a8fda36feea63864a
Brendan Reid <36493852+BrendanReid99@users.noreply.github.com>
2023-03-22 20:10:19 +0000

    added a current day cycle and box colliders for walk thru able objects

--- ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
+++ ComponentsPrototype/Assets/Scripts/DayAndNightCycler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using System.Diagnostics.Tracing;
+using System.Collections.Generic;
 
 public class DayAndNightCycler : MonoBehaviour
 {
@@ -14,8 +15,15 @@ public class DayAndNightCycler : MonoBehaviour
     private Vector3 rotationAxis;
 
     public TextMeshProUGUI timeOfDay;
+    public TextMeshProUGUI dayofWeek;
+
     public float currentTime;
-    int counter;
+    //private string[] days_in_week = { "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun" };
+    public int currentDay;
+    int time_counter;
+    //int day_counter;
+
+
 
 
     private void Awake()
@@ -27,12 +35,20 @@ public class DayAndNightCycler : MonoBehaviour
         starsRefreshRate = 0.1f;
         rotationAxis = starsTransform.right;
         rotationAngleStep = 360.0f * starsRefreshRate / gameParameters.lengthOfDayInSeconds;
+
+        //traverse through the array to give each day an index number
+        //day_counter = 0;
+        
+        //for (int i = 0; i < days_in_week.Length; i++) {
+           //day_counter++;
+        //}
     }
 
     void Start()
     {
         //set current time in game (360 minutes = 6am in game time i.e., 60 (00:00am) * 6 (hrs) = 360)
         currentTime = 360;
+        currentDay = 1;
 
 
         StartCoroutine("UpdateStars");
@@ -40,21 +56,24 @@ public class DayAndNightCycler : MonoBehaviour
 
     void FixedUpdate() //EVERY 2 FRAMES == 1 MINUTE (GAME TIME)
     {
-        counter += 1;
-        if (counter % 4 == 0) { //modulo 4 here as length of day is set to 120 (2 minutes), if length of day was set to 60 (1 minute) we would change to modulo 2 (as fixedUpdate is every 2 frames)
+        time_counter += 1;
+        if (time_counter % 4 == 0) { //modulo 4 here as length of day is set to 120 (2 minutes), if length of day was set to 60 (1 minute) we would change to modulo 2 (as fixedUpdate is every 2 frames)
             currentTime += 1;   //update the current time by 1 minute
         }
 
         float hours = Mathf.FloorToInt(currentTime / 60);
         float minutes = Mathf.FloorToInt(currentTime % 60);
+        float day = Mathf.FloorToInt(currentDay); 
 
         //resets clock to 00 hrs if greater than 24
         if (hours >= 24) {
             hours = 0;
             currentTime = 0;
+            currentDay++;
         }
 
-        timeOfDay.text = string.Format("{0:00}:{1:00}", hours,  minutes); //converts text into hours and minutes format
+        timeOfDay.text = string.Format("{0:00} Hrs: {1:00} Mins", hours,  minutes); //converts text into hours and minutes format
+        dayofWeek.text = string.Format("Day: {0},", day); //converts text into current day of week
 
     }
 
